f4da66a93d995c5b26da74e831005b3413881a4f
Vinnydaninja <vincenguyen35@gmail.com>
2022-12-08 01:04:49 -0800

    Add basic JS for vote requests

---
+++ public/js/cities_show.js
@@ -0,0 +1,25 @@
+const upvoteBtn = document.getElementById("upvote_btn");
+const downvoteBtn = document.getElementById("downvote_btn");
+
+upvoteBtn.addEventListener("click", async function() {
+    // Build fetch options
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({vote: "up"})
+    }
+
+    // Send fetch request
+    await fetch("/cities/vote", options)
+    .then(data => {
+        return data.json();
+    })
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
