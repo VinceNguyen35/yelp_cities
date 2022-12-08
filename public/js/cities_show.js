29fba45c11ba1de6dd4811b1052373089f67f5ef
Vinnydaninja <vincenguyen35@gmail.com>
2022-12-08 01:34:03 -0800

    Refactor vote function

--- public/js/cities_show.js
+++ public/js/cities_show.js
@@ -1,14 +1,29 @@
+/////////////////////////////////////////////////////////////////////////
+// SELECT ELEMENTS
+/////////////////////////////////////////////////////////////////////////
+
 const upvoteBtn = document.getElementById("upvote_btn");
 const downvoteBtn = document.getElementById("downvote_btn");
 
-upvoteBtn.addEventListener("click", async function() {
+/////////////////////////////////////////////////////////////////////////
+// HELPER FUNCTIONS
+/////////////////////////////////////////////////////////////////////////
+
+const sendVote = async (voteType) => {
     // Build fetch options
     const options = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
-        },
-        body: JSON.stringify({vote: "up"})
+        }
+    }
+
+    if(voteType === "up") {
+        options.body = JSON.stringify({vote: "up"});
+    } else if(voteType === "down") {
+        options.body = JSON.stringify({vote: "down"});
+    } else {
+        throw "voteType must be 'up' or 'down'";
     }
 
     // Send fetch request
@@ -22,4 +37,16 @@ upvoteBtn.addEventListener("click", async function() {
     .catch(err => {
         console.log(err);
     });
+}
+
+/////////////////////////////////////////////////////////////////////////
+// ADD EVENT LISTENERS
+/////////////////////////////////////////////////////////////////////////
+
+upvoteBtn.addEventListener("click", async function() {
+    sendVote("up");
+});
+
+downvoteBtn.addEventListener("click", async function() {
+    sendVote("down");
 });
